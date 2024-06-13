53d2ccba001513a4e741625216c6fd25120c00b9
oriyaPerel <159001341+oriyaPerel@users.noreply.github.com>
2024-06-13 15:40:32 +0300

    Update Algorithms.hpp

--- Algorithms.hpp
+++ Algorithms.hpp
@@ -10,10 +10,9 @@
 
 namespace Algorithms
 {
-
-        bool isConnected(const ariel::Graph &g);
-        int shortestPath(const ariel::Graph &g, size_t start, size_t end);
-        std::string isContainsCycle(const ariel::Graph &g);
-        std::string isBipartite(const ariel::Graph &g);
-        std::string negativeCycle(const ariel::Graph &g);
+        bool isConnected(const ariel::Graph &g);                                  // check if the graph is connected
+        std::string shortestPath(const ariel::Graph &g, size_t src, size_t dest); // return the shortest path between two vertices
+        std::string isContainsCycle(const ariel::Graph &g);                       // check if the graph contains a cycle
+        std::string isBipartite(const ariel::Graph &g);                           // check if the graph is bipartite
+        std::string negativeCycle(const ariel::Graph &g);                         // check if the graph contains a negative cycle
 }
