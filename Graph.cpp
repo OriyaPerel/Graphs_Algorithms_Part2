fe1056a4eeee537905da761e064b71b5afe0ae03
oriyaPerel <159001341+oriyaPerel@users.noreply.github.com>
2024-06-13 15:39:13 +0300

    Update Graph.cpp

--- Graph.cpp
+++ Graph.cpp
@@ -8,7 +8,7 @@
 namespace ariel
 {
     Graph::Graph()
-    { // default constructor
+    {
         this->directed = true;
     }
 
@@ -20,8 +20,7 @@ namespace ariel
     Graph::Graph(std::vector<std::vector<int>> matrix)
     {
         this->AdjacencyMatrix = matrix;
-        this->row = matrix.size();
-        this->col = matrix[0].size();
+        this->numVertices = matrix.size();
         this->directed = true;
     }
 
@@ -36,14 +35,16 @@ namespace ariel
     }
 
     int Graph::getNumEdge() const
+    // return the number of edges in the graph if the graph is directed return the number of edges
+    // if the graph is undirected return the number of edges divided by 2
     {
         int count = 0;
-        int V = this->col;
+        int V = this->numVertices;
         for (size_t i = 0; i < V; i++)
         {
             for (size_t j = 0; j < V; j++)
             {
-                if (AdjacencyMatrix[i][j] != 0)
+                if (AdjacencyMatrix[i][j] != 0) // if the edge is exist
                 {
                     count++;
                 }
@@ -60,63 +61,58 @@ namespace ariel
     {
         return this->AdjacencyMatrix.size();
     }
+
     void Graph::printGraph() const
     {
-        int count = 0;
-        int V = this->col;
+        int V = this->numVertices;
+        int E = this->getNumEdge();
         for (size_t i = 0; i < V; i++)
         {
             for (size_t j = 0; j < V; j++)
             {
                 std::cout << AdjacencyMatrix[i][j] << " ";
-                if (AdjacencyMatrix[i][j] != 0)
-                {
-                    count++;
-                }
             }
             std::cout << std::endl;
         }
         if (this->directed == true)
         {
-            std::cout << "Graph with " << row << " vertices and " << count << " edges" << std::endl;
+            std::cout << "Graph with " << numVertices << " vertices and " << E << " edges" << std::endl;
         }
         else
         {
-            std::cout << "Graph with " << row << " vertices and " << count / 2 << " edges" << std::endl;
+            std::cout << "Graph with " << numVertices << " vertices and " << E / 2 << " edges" << std::endl;
         }
     }
 
-    void Graph::loadGraph(std::vector<std::vector<int>> ingraph)
+    void Graph::loadGraph(std::vector<std::vector<int>> inputgraph)
     {
-        if (ingraph.empty())
+        if (inputgraph.empty())
         {
             throw std::invalid_argument("Invalid graph: The graph is empty");
             return;
         }
-        size_t r = ingraph.size();
-        size_t c = ingraph[0].size();
+        size_t r = inputgraph.size();
 
         for (size_t i = 0; i < r; ++i)
         {
-            if (ingraph[i].size() != r)
+            if (inputgraph[i].size() != r)
             {
                 throw std::invalid_argument("Invalid graph: The graph is not a square matrix.");
                 return;
             }
         }
-        this->AdjacencyMatrix = ingraph;
-        this->row = r;
-        this->col = c;
+        this->AdjacencyMatrix = inputgraph;
+        this->numVertices = r;
     }
 
     std::string Graph::toString() const
     {
         std::stringstream output;
 
-        for (size_t i = 0; i < row; i++)
+        for (size_t i = 0; i < numVertices; i++)
         {
             output << "[";
-            for (size_t j = 0; j < col; j++)
+            for (size_t j = 0; j < numVertices; j++)
             {
                 if (j != 0)
                 {
@@ -132,30 +128,31 @@ namespace ariel
 
     Graph Graph::operator+(const Graph &other)
     {
-        if (this->col != other.col || this->row != other.row)
+        if (this->numVertices != other.numVertices) // if the size of the graphs is different we can't add them
         {
             throw std::invalid_argument("can't add two graphs with different size");
         }
-        std::vector<std::vector<int>> g((size_t)this->row, std::vector<int>((size_t)this->col));
-        for (size_t i = 0; i < this->row; i++)
+        std::vector<std::vector<int>> g((size_t)this->numVertices, std::vector<int>((size_t)this->numVertices));
+        for (size_t i = 0; i < this->numVertices; i++)
         {
-            for (size_t j = 0; j < this->col; j++)
+            for (size_t j = 0; j < this->numVertices; j++)
             {
                 g[i][j] = this->getGraph()[i][j] + other.getGraph()[i][j];
             }
         }
         return Graph(g);
     }
+    
     Graph Graph::operator-(const Graph &other)
     {
-        if (this->col != other.col || this->row != other.row)
+        if (this->numVertices != other.numVertices) // if the size of the graphs is different we can't subtract them
         {
             throw std::invalid_argument("can't add two graphs with different size");
         }
-        std::vector<std::vector<int>> result((size_t)this->row, std::vector<int>((size_t)this->col));
-        for (size_t i = 0; i < this->row; i++)
+        std::vector<std::vector<int>> result((size_t)this->numVertices, std::vector<int>((size_t)this->numVertices));
+        for (size_t i = 0; i < this->numVertices; i++)
         {
-            for (size_t j = 0; j < this->col; j++)
+            for (size_t j = 0; j < this->numVertices; j++)
             {
                 result[i][j] = this->getGraph()[i][j] - other.getGraph()[i][j];
             }
@@ -163,48 +160,34 @@ namespace ariel
         return Graph(result);
     }
 
-    Graph Graph::operator*(const Graph &other)
-    {
-        // Check if the dimensions of the graphs are compatible for multiplication
-        if (this->col != other.row)
-        {
-            throw std::invalid_argument("Can not multiply graphs with differnt size");
-        }
-
-        std::vector<std::vector<int>> result((size_t)this->row, std::vector<int>((size_t)other.col));
-        for (size_t i = 0; i < this->row; ++i)
-        {
-            for (size_t j = 0; j < other.col; ++j)
-            {
-                for (size_t k = 0; k < this->col; ++k)
-                {
-                    result[i][j] += this->AdjacencyMatrix[i][k] * other.AdjacencyMatrix[k][j];
-                }
-            }
-        }
-        for (size_t i = 0; i < this->row; i++)
-        {
-            result[i][i] = 0;
-        }
-
-        return Graph(result);
-    }
-
-    Graph &Graph::operator-()
+    Graph &Graph::operator-() // unary operator  -g
     {
         return *this *= (-1);
     }
 
-    Graph &Graph::operator+()
+    Graph &Graph::operator+() // unary operator +g
     {
         return *this *= 1;
     }
 
+    Graph &Graph::operator/=(int num)
+    {
+
+        for (size_t i = 0; i < numVertices; i++)
+        {
+            for (size_t j = 0; j < numVertices; j++)
+            {
+                this->AdjacencyMatrix[i][j] /= num;
+            }
+        }
+        return *this;
+    }
+
     Graph &Graph::operator+=(int scalar)
     {
-        for (size_t i = 0; i < this->row; ++i)
+        for (size_t i = 0; i < this->numVertices; ++i)
         {
-            for (size_t j = 0; j < this->col; ++j)
+            for (size_t j = 0; j < this->numVertices; ++j)
             {
                 if (this->AdjacencyMatrix[i][j] != 0)
                 { // add scalar only to edge that existed
@@ -216,12 +199,12 @@ namespace ariel
     }
     Graph &Graph::operator-=(int scalar)
     {
-        for (size_t i = 0; i < this->row; ++i)
+        for (size_t i = 0; i < this->numVertices; ++i)
         {
-            for (size_t j = 0; j < this->col; ++j)
+            for (size_t j = 0; j < this->numVertices; ++j)
             {
                 if (this->AdjacencyMatrix[i][j] != 0)
-                { // add scalar only to edge that existed
+                { // sub scalar only to edge that existed
                     this->AdjacencyMatrix[i][j] -= scalar;
                 }
             }
@@ -231,31 +214,36 @@ namespace ariel
 
     Graph &Graph::operator++()
     {
-        return *this += 1;
+        return *this += 1; // using the += operator
     }
 
     Graph &Graph::operator--()
     {
-        return *this -= 1;
+        return *this -= 1; // using the -= operator
     }
 
-    Graph &Graph::operator++(int)
+    Graph Graph::operator++(int num) // g++
     {
-        return *this += 1;
+        Graph oldGrap;                      // copy the graph
+        oldGrap.loadGraph(AdjacencyMatrix); // load the graph
+        *this += (1);                       // using the += operator and add 1 to the graph
+        return oldGrap;                     // return the old graph
     }
-
-    Graph &Graph::operator--(int)
+    Graph Graph::operator--(int num) // g--
     {
-        return *this -= 1;
+        Graph oldGrap;                      // copy the graph
+        oldGrap.loadGraph(AdjacencyMatrix); // load the graph
+        *this -= (1);                       // using the -= operator and subtract 1 from the graph
+        return oldGrap;                     // return the old graph
     }
 
     bool Graph::operator==(const Graph &other)
     {
-        if (this->row == other.row) // the matrix in the seem size
+        if (this->numVertices == other.numVertices) // the matrix in the seem size and the same values
         {
-            for (size_t i = 0; i < this->row; i++)
+            for (size_t i = 0; i < this->numVertices; i++)
             {
-                for (size_t j = 0; j < this->col; j++)
+                for (size_t j = 0; j < this->numVertices; j++)
                 {
                     if (this->AdjacencyMatrix[i][j] != other.AdjacencyMatrix[i][j])
                     {
@@ -266,7 +254,7 @@ namespace ariel
             return true;
         }
 
-        else if (!(*this < other) && !(*this > other))
+        else if (!(*this < other) && !(*this > other)) // if the graphs are not equal and the size is different
         {
             return true;
         }
@@ -276,11 +264,11 @@ namespace ariel
 
     bool Graph::operator!=(const Graph &other)
     {
-        if (this->row == other.row) // the matrix in the seem size
+        if (this->numVertices == other.numVertices) // the matrix in the seem size
         {
-            for (size_t i = 0; i < this->row; i++)
+            for (size_t i = 0; i < this->numVertices; i++)
             {
-                for (size_t j = 0; j < this->col; j++)
+                for (size_t j = 0; j < this->numVertices; j++)
                 {
                     if (this->AdjacencyMatrix[i][j] != other.AdjacencyMatrix[i][j])
                     {
@@ -291,7 +279,7 @@ namespace ariel
             return false;
         }
 
-        else if (this->getNumEdge() != other.getNumEdge())
+        else if (this->getNumEdge() != other.getNumEdge()) // if the number of edges is different
         {
             return true;
         }
@@ -299,8 +287,8 @@ namespace ariel
     }
 
     bool contain(const Graph &g1, const Graph &g2)
-    { // if g1 contained in g2
-        if (g1.getNumVertices() > g2.getNumVertices())
+    {                                                  // if g1 contained in g2 (g1<g2)
+        if (g1.getNumVertices() > g2.getNumVertices()) // if the size of g1 is bigger than g2 it can't be contained
         {
             return false;
         }
@@ -309,7 +297,7 @@ namespace ariel
         {
             for (size_t j = 0; j < g1.getNumVertices(); j++) // Iterate over the columns of this
             {
-                if (g1.getGraph()[i][j] != 0 && g2.getGraph()[i][j] != 0)
+                if (g1.getGraph()[i][j] != 0 && g2.getGraph()[i][j] == 0) // if the edge in g1 is not in g2
                 {
                     return false;
                 }
@@ -327,14 +315,14 @@ namespace ariel
         else if (!contain(*this, other) && !contain(other, this) && *this != other)
         {
             // if neither of the graphs is conteined by the other and
-            // they aren't equal we need to check the size
+            // they aren't equal we need to check the number of edges
             if (this->getNumEdge() > other.getNumEdge())
             {
                 return true;
             }
             else if (this->getNumEdge() == other.getNumEdge())
             {
-                if (this->col > other.col)
+                if (this->numVertices > other.numVertices)
                 {
                     return true;
                 }
@@ -343,29 +331,9 @@ namespace ariel
         return false;
     }
 
-    bool Graph::operator<(const Graph &other) // this<other
+    bool Graph::operator<(const Graph &other)
     {
-        if (contain(*this, other) && *this != other) // if this contianed in this -> this<other
-        {
-            return true;
-        }
-        else if (!contain(*this, other) && !contain(other, *this) && *this != other)
-        {
-            // if neither of the graphs is conteined by the other and
-            // they aren't equal we need to check the size
-            if (other.getNumEdge() > this->getNumEdge())
-            {
-                return true;
-            }
-            else if (this->getNumEdge() == other.getNumEdge())
-            {
-                if (other.col > this->col)
-                {
-                    return true;
-                }
-            }
-        }
-        return false;
+        return (*this != other) && (!(*this > other));
     }
 
     bool Graph::operator<=(const Graph &other)
@@ -389,10 +357,10 @@ namespace ariel
     std::ostream &operator<<(std::ostream &output, const Graph &g)
     {
         output << "graph" << "\n";
-        for (size_t i = 0; i < g.row; i++)
+        for (size_t i = 0; i < g.numVertices; i++)
         {
             output << "[";
-            for (size_t j = 0; j < g.col; j++)
+            for (size_t j = 0; j < g.numVertices; j++)
             {
                 if (j != 0)
                 {
@@ -408,9 +376,9 @@ namespace ariel
 
     Graph &Graph::operator*=(int scalar)
     {
-        for (size_t i = 0; i < row; ++i)
+        for (size_t i = 0; i < numVertices; ++i)
         {
-            for (size_t j = 0; j < col; ++j)
+            for (size_t j = 0; j < numVertices; ++j)
             {
                 this->AdjacencyMatrix[i][j] *= scalar;
             }
@@ -420,9 +388,9 @@ namespace ariel
 
     Graph &Graph::operator+=(const Graph &other)
     {
-        for (size_t i = 0; i < row; ++i)
+        for (size_t i = 0; i < numVertices; ++i)
         {
-            for (size_t j = 0; j < col; ++j)
+            for (size_t j = 0; j < numVertices; ++j)
             {
                 this->AdjacencyMatrix[i][j] += other.AdjacencyMatrix[i][j];
             }
@@ -432,14 +400,41 @@ namespace ariel
 
     Graph &Graph::operator-=(const Graph &other)
     {
-        for (size_t i = 0; i < row; ++i)
+        for (size_t i = 0; i < numVertices; ++i)
         {
-            for (size_t j = 0; j < col; ++j)
+            for (size_t j = 0; j < numVertices; ++j)
             {
                 this->AdjacencyMatrix[i][j] -= other.AdjacencyMatrix[i][j];
             }
         }
         return *this; // Return the modified graph
+    }
+
+    Graph Graph::operator*(const Graph &other)
+    {
+        // Check if the dimensions of the graphs are compatible for multiplication
+        if (this->numVertices != other.numVertices)
+        {
+            throw std::invalid_argument("Can not multiply graphs with differnt size");
+        }
+
+        std::vector<std::vector<int>> result((size_t)this->numVertices, std::vector<int>((size_t)other.numVertices));
+        for (size_t i = 0; i < this->numVertices; ++i)
+        {
+            for (size_t j = 0; j < other.numVertices; ++j)
+            {
+                for (size_t k = 0; k < this->numVertices; ++k)
+                {
+                    result[i][j] += this->AdjacencyMatrix[i][k] * other.AdjacencyMatrix[k][j];
+                }
+            }
+        }
+        for (size_t i = 0; i < this->numVertices; i++) // the diagonal should be 0
+        {
+            result[i][i] = 0;
+        }
+
+        return Graph(result); // Return the resulting graph
     }
 
 }
